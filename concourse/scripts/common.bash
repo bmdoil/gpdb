f6539502137c02c6698efa680d28bbb43aa57072
Brent Doil <bdoil@vmware.com>
2020-12-17 16:14:36 -0500

    DO NOT MERGE INTO 6X_STABLE
    - Update gen_pipeline.py to include photon3 as OS option
    - Update template to add photon3 resources and jobs
    - Update compile task and script to include photon3
    - Add photon specific config flags to gpAux/Makefile and
    gpAux/Makefile.global
    - Add photon platform testing and update scripts.
    
    Authored-by: Brent Doil <bdoil@vmware.com>

--- concourse/scripts/common.bash
+++ concourse/scripts/common.bash
@@ -44,6 +44,27 @@ function build_arch() {
 	echo "${id}${version}_x86_64"
 }
 
+function determine_os() {
+  local name version
+  if [ -f /etc/redhat-release ]; then
+    name="centos"
+    version=$(sed </etc/redhat-release 's/.*release *//' | cut -f1 -d.)
+  elif [ -f /etc/SuSE-release ]; then
+    name="sles"
+    version=$(awk -F " *= *" '$1 == "VERSION" { print $2 }' /etc/SuSE-release)
+  elif  grep -q photon /etc/os-release  ; then
+    name="photon"
+    version=$( awk -F " *= *" '$1 == "VERSION_ID" { print $2 }' /etc/os-release | cut -f1 -d.)
+  elif grep -q ubuntu /etc/os-release ; then
+    name="ubuntu"
+    version=$(awk -F " *= *" '$1 == "VERSION_ID" { print $2 }' /etc/os-release | tr -d \")
+  else
+    echo "Could not determine operating system type" >/dev/stderr
+    exit 1
+  fi
+  echo "${name}${version}"
+}
+
 ## ----------------------------------------------------------------------
 ## Test functions
 ## ----------------------------------------------------------------------
